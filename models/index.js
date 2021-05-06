8f627db8cd8ee8ba39ea414240eae057b216b1a0
JPB <jbanas9124@gmail.com>
2021-05-05 23:28:26 -0500

    add voting routes

--- models/index.js
+++ models/index.js
@@ -1,13 +1,57 @@
-const User = require('./User');
 const Post = require('./Post');
+const User = require('./User');
+const Vote = require('./Vote');
 
 //create associations
+
+//user has many posts connected through
+//Post foreign key 'user_id'
 User.hasMany(Post, {
   foreignKey: 'user_id'
 });
 
+//Post only has one connection to User in this instance
+//through foreign key 'user_id'
 Post.belongsTo(User, {
   foreignKey: 'user_id',
 });
 
-module.exports = { User, Post };
+//connection between User & Post via
+//Vote through foreign key 'user_id'
+//Vote is referenced as 'voted_posts'
+User.belongsToMany(Post, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'user_id'
+});
+
+//connection between User & Post via
+//Vote through foreign key 'user_id'
+//Vote is referenced as 'voted_posts'
+Post.belongsToMany(User, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'post_id'
+});
+
+//Vote's one connection to User
+Vote.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+//Vote's one connection to Post
+Vote.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+//User can have multiple Vote
+User.hasMany(Vote, {
+  foreignKey: 'user_id'
+});
+
+//Post can have multiple votes
+Post.hasMany(Vote, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote };
