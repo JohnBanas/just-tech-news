db9ec2cb5c9d32611b076c828ce36270dce1fbaf
JPB <jbanas9124@gmail.com>
2021-05-06 00:21:36 -0500

    created api endpoints for comments

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const Post = require('./Post');
 const User = require('./User');
 const Vote = require('./Vote');
-
+const Comment = require('./Comment');
 //create associations
 
 //user has many posts connected through
@@ -54,4 +54,20 @@ Post.hasMany(Vote, {
   foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Vote };
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comment };
