46030f5d3df78e2b3da15420c098543968999a46
JPB <jbanas9124@gmail.com>
2021-05-11 20:44:08 -0500

    edit and delete function for comments

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const exphbs = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/connection');
+const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -19,9 +19,9 @@ const sess = {
   })
 };
 
-const helpers = require('./utils/helpers');
-
 app.use(session(sess));
+
+const helpers = require('./utils/helpers');
 
 const hbs = exphbs.create({ helpers });
 
